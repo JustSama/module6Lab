f1f94ff2bf2d281f36a48206d575fc3645ebbc0f
JustSama <106009367+JustSama@users.noreply.github.com>
2024-10-16 23:52:58 +0500

    Add files via upload

---
+++ Lab/Prototype.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IPrototype
+{
+    IPrototype Clone();
+}
+
+public class Section : IPrototype
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public IPrototype Clone()
+    {
+        return new Section { Title = Title, Content = Content };
+    }
+}
+
+public class Image : IPrototype
+{
+    public string URL { get; set; }
+
+    public IPrototype Clone()
+    {
+        return new Image { URL = URL };
+    }
+}
+
+public class Document : IPrototype
+{
+    public string Title { get; set; }
+    public List<Section> Sections { get; set; } = new List<Section>();
+    public List<Image> Images { get; set; } = new List<Image>();
+
+    public IPrototype Clone()
+    {
+        Document clone = new Document
+        {
+            Title = Title
+        };
+        foreach (var section in Sections)
+        {
+            clone.Sections.Add((Section)section.Clone());
+        }
+        foreach (var image in Images)
+        {
+            clone.Images.Add((Image)image.Clone());
+        }
+        return clone;
+    }
+}
+
+public class DocumentManager
+{
+    public IPrototype CreateDocument(IPrototype prototype)
+    {
+        return prototype.Clone();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentManager manager = new DocumentManager();
+
+        Document originalDocument = new Document
+        {
+            Title = "Документ 1"
+        };
+        originalDocument.Sections.Add(new Section { Title = "Раздел 1", Content = "Содержимое раздела 1" });
+        originalDocument.Images.Add(new Image { URL = "http://example.com/image1.png" });
+
+        Document clonedDocument = (Document)manager.CreateDocument(originalDocument);
+        clonedDocument.Title = "Клонированный документ 1";
+        clonedDocument.Sections[0].Content = "Измененное содержимое раздела 1";
+        clonedDocument.Images.Add(new Image { URL = "http://example.com/image2.png" });
+
+        Console.WriteLine($"Оригинальный документ: {originalDocument.Title}");
+        foreach (var section in originalDocument.Sections)
+        {
+            Console.WriteLine($"Раздел: {section.Title}, Содержимое: {section.Content}");
+        }
+        foreach (var image in originalDocument.Images)
+        {
+            Console.WriteLine($"Изображение: {image.URL}");
+        }
+
+        Console.WriteLine($"\nКлонированный документ: {clonedDocument.Title}");
+        foreach (var section in clonedDocument.Sections)
+        {
+            Console.WriteLine($"Раздел: {section.Title}, Содержимое: {section.Content}");
+        }
+        foreach (var image in clonedDocument.Images)
+        {
+            Console.WriteLine($"Изображение: {image.URL}");
+        }
+    }
+}
---
+++ Lab/Singleton.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+public enum LogLevel
+{
+    INFO,
+    WARNING,
+    ERROR
+}
+
+public class Logger
+{
+    private static Logger instance;
+    private static readonly object padlock = new object();
+    private LogLevel logLevel = LogLevel.INFO;
+    private string logFilePath = "log.txt";
+
+    private Logger() { }
+
+    public static Logger GetInstance()
+    {
+        lock (padlock)
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+    }
+
+    public void SetLogLevel(LogLevel level)
+    {
+        logLevel = level;
+    }
+
+    public void SetLogFilePath(string path)
+    {
+        logFilePath = path;
+    }
+
+    public void Log(string message, LogLevel level)
+    {
+        if (level >= logLevel)
+        {
+            lock (padlock)
+            {
+                File.AppendAllText(logFilePath, $"{DateTime.Now}: [{level}] {message}{Environment.NewLine}");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Logger logger = Logger.GetInstance();
+        logger.SetLogLevel(LogLevel.INFO);
+
+        Thread thread1 = new Thread(() =>
+        {
+            logger.Log("Thread 1: Information message.", LogLevel.INFO);
+            logger.Log("Thread 1: Warning message.", LogLevel.WARNING);
+            logger.Log("Thread 1: Error message.", LogLevel.ERROR);
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            logger.Log("Thread 2: Information message.", LogLevel.INFO);
+            logger.Log("Thread 2: Warning message.", LogLevel.WARNING);
+            logger.Log("Thread 2: Error message.", LogLevel.ERROR);
+        });
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Logging completed.");
+    }
+}
